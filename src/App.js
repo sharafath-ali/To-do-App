bddbb5a6026c1eb02f9957e0a3d2a4dc363a2447
sharafath ali <sharafathaliofficial@gmail.com>
2024-12-08 21:01:37 +0530

    added localhost feature

--- src/App.js
+++ src/App.js
@@ -4,30 +4,35 @@ import { useState } from 'react';
 import List from './List';
 
 function App() {
-  const [todolist, settodolist] = useState([]);
+  const [todolist, settodolist] = useState(JSON.parse(localStorage.getItem('todolist')));
   const [newtask, setnewtask] = useState('');
 
-  const Addtask = () => {
-    if (newtask.trim() !== '') {
-      const task = {
+  const addTask = () => {
+    const trimmedTask = newtask.trim();
+    if (trimmedTask !== '') {
+      const newTask = {
         id: todolist.length === 0 ? 1 : todolist[todolist.length - 1].id + 1,
-        task: newtask.trim(),
+        task: trimmedTask,
         color: '#f9f9f9',
       };
-      settodolist([...todolist, task]);
+      settodolist([...todolist, newTask]);
       setnewtask('');
+      localStorage.setItem('todolist', JSON.stringify([...todolist, newTask]));
     }
   };
 
-  const Colorchange = (id) => {
-    const newlist = todolist.map((element) =>
-      element.id === id ? { ...element, color: '#d4edda' } : element
+  const changeTaskColor = (id) => {
+    const updatedList = todolist.map((task) =>
+      task.id === id ? { ...task, color: '#d4edda' } : task
     );
-    settodolist(newlist);
+    settodolist(updatedList);
+    localStorage.setItem('todolist', JSON.stringify(updatedList));
   };
 
-  const Delete = (id) => {
-    settodolist(todolist.filter((element) => element.id !== id));
+  const deleteTask = (id) => {
+    const updatedList = todolist.filter(task => task.id !== id);
+    settodolist(updatedList);
+    localStorage.setItem('todolist', JSON.stringify(updatedList));
   };
 
   return (
@@ -39,7 +44,7 @@ function App() {
           placeholder="Write a task..."
           onChange={(e) => setnewtask(e.target.value)}
         />
-        <Button variant="info" size="sm" onClick={Addtask}>
+        <Button variant="info" size="sm" onClick={addTask}>
           Add Task
         </Button>
       </div>
@@ -55,8 +60,8 @@ function App() {
               <List
                 key={key}
                 element={element}
-                Delete={Delete}
-                Colorchange={Colorchange}
+                Delete={deleteTask}
+                Colorchange={changeTaskColor}
               />
             );
           })
--- src/List.js
+++ src/List.js
@@ -4,7 +4,7 @@ import { Button } from 'react-bootstrap';
 function List({ element, Delete, Colorchange }) {
   return (
     <div className="task-item" style={{ backgroundColor: element.color }}>
-      <h6>{element.task}</h6>
+      <h6 className="task-text">{element.task}</h6>
       <div>
         <Button
           variant="outline-danger"
